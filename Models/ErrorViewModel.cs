0cee4b72920fd97f5d50f836542d62446d84ca2b
Jessica Gonzales <jgonzales0688@gmail.com>
2023-01-23 14:45:52 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlashCardMania.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
